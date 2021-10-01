0397c24e9eb96c1f675aa574786903506576c97b
dangviet610 <dahovi6102002@gmail.com>
2021-10-01 15:04:25 +0700

    Add files via upload

--- Final_project.py
+++ Final_project.py
@@ -1,1 +1,37 @@
+# import visualization libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# read data
+price_rd = pd.read_csv('price_paid_records_small.csv')
+
+# display the first 5 rows in data
+print(price_rd.head(5))
+
+# min, max, mean of data
+des_data = price_rd.describe()
+print('Describe the Price paid ', des_data)
+
+# Count the number of County in Dataframe
+cnt_county = pd.value_counts(price_rd['County'])
+print('The number of Country in DataFrame', cnt_county)
+
+# create a new column Year from the first 4 characters of the Date of Transfer feature
+price_rd['Year'] = price_rd['Date of Transfer'].str[:4]
+print(price_rd)
+
+# List the high price above 500000 in Town/City
+lst = price_rd[['Price', 'Old/New', 'Town/City']]
+hgh_pr = lst[(lst['Price'] >= 500000)]
+print(hgh_pr)
+
+# Draw chart about Price
+plt.hist(price_rd['Price'])
+plt.title('All House Prices Distribution')
+plt.xlabel('House Price (£)')
+plt.ylabel('Count')
+plt.show()
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 "# Final_exam" 
+
+
+# Name: Dang_Hoang_Viet
+## Id_Student: 20020018
+
+# Data:
+## price_paid_records.small
+## price_paid_records.medium
+
+# Questions:
+### Ques 1: import visualization libraries
+### Ques 2: read Dataframe
+### Ques 3: display the first 5 rows in data
+### Ques 4: min, max, mean of data
+### Ques 5: Count the number of County in Dataframe
+### Ques 6: Create a new column Year from the first 4 characters of the Date of Transfer feature
+### Ques 7: List the high price above 500000 in Town/City
+### Ques 8: Draw chart about Price
+### Ques 9: Draw pie chart to display rate the people paid to live in Town/City
+### Ques 10: Draw 3 pie charts to compare three columns 'Town/City', 'District', 'County'
